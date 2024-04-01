7320bf099dfded8987d66601561cb2a5f002c088
Talha Anwar <mtalhaanwar359@gmail.com>
2024-04-01 23:36:15 +0500

    Fixing the Scrollbar issue

--- src/Position/index.jsx
+++ src/Position/index.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Positions() {
   return (
-    <div className='h-screen '>
+    <div className='flex flex-col justify-between h-screen overflow-hidden'>
         <div className='flex justify-around ' id="colorBox">
-            <div className="w-2/5	 h-20 bg-blue-500	"></div>
-            <div className="w-2/5	 h-20 bg-green-500"></div>
+            <div className="w-2/5 h-20 bg-blue-500"></div>
+            <div className="w-2/5 h-20 bg-green-500"></div>
         </div>
-        <div className="flex justify-center items-center w-full h-full">
-        <div className='flex items-center w-2/4	 h-2/4 bg-orange-400	 justify-around' id="mainBoxes">
+        <div className="flex justify-center items-center flex-grow" >
+        <div className='flex items-center w-2/4 h-3/4 bg-orange-400 justify-around' id="mainBoxes">
             <div className='flex-col space-y-8 ' id="3boxes">
                 <div className="h-20 w-20 bg-slate-600"></div>
                 <div className="h-20 w-20 bg-slate-600"></div>
@@ -24,9 +24,9 @@ function Positions() {
             </div>
         </div>
         </div>
-        <div className='flex justify-between absolute inset-x-0 end-0 ' id="footer">
-            <div className="h-20 w-1/4	 bg-blue-600 "></div>
-            <div className="h-20 w-1/4	 bg-yellow-600"></div>
+        <div className='flex justify-between ' id="footer">
+            <div className="h-20 w-1/4 bg-blue-600 "></div>
+            <div className="h-20 w-1/4 bg-yellow-600"></div>
         </div>
     </div>
   )
