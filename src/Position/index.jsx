d424a5999cec5cf625d54041e653efee766f784d
Talha Anwar <mtalhaanwar359@gmail.com>
2024-04-01 16:24:22 +0500

    Positions

---
+++ src/Position/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Positions() {
+  return (
+    <div>
+        <div className='flex justify-around ' id="colorBox">
+            <div className="w-2/5	 h-20 bg-blue-500	"></div>
+            <div className="w-2/5	 h-20 bg-green-500"></div>
+        </div>
+        <div className="flex justify-center items-center w-screen h-screen">
+        <div className='flex items-center w-2/4	 h-2/4 bg-orange-400	 justify-around' id="mainBoxes">
+            <div className='flex-col space-y-8 ' id="3boxes">
+                <div className="h-20 w-20 bg-slate-600"></div>
+                <div className="h-20 w-20 bg-slate-600"></div>
+                <div className="h-20 w-20 bg-slate-600"></div>
+            </div>
+            <div className='space-y-8' id="2boxes">
+                <div className="h-20 w-20 bg-green-600"></div>
+                <div className="h-20 w-20 bg-green-600"></div>
+            </div>
+            <div className='space-y-8' id="2boxes">
+                <div className="h-20 w-20 bg-red-600"></div>
+                <div className="h-20 w-20 bg-red-600"></div>
+            </div>
+        </div>
+        </div>
+        <div className='flex justify-between absolute inset-x-0 end-0 ' id="footer">
+            <div className="h-20 w-1/4	 bg-blue-600 "></div>
+            <div className="h-20 w-1/4	 bg-yellow-600"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Positions
--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Index29 from './29/index.jsx'
+// import Index29 from './29/index.jsx'
+import Positions from './Position/index.jsx'
 // import ToDoList from './ToDoList/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Index29/>
+    <Positions></Positions>
+    {/* <Index29/> */}
       {/* <ToDoList/> */}
     {/* <App /> */}
   </React.StrictMode>,
