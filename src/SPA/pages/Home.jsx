d9ff8adba077abd1e4af00093fb783a28548a45b
Talha Anwar <mtalhaanwar359@gmail.com>
2024-04-17 16:02:40 +0500

    nothing

--- src/SPA/pages/Home.jsx
+++ src/SPA/pages/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
-  return(
-    <div>Home</div>
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h2>Home</h2>
+      <button onClick={() => navigate('/about')}>Go to About</button>
+    </div>
   );
 }
-
 export default Home;
